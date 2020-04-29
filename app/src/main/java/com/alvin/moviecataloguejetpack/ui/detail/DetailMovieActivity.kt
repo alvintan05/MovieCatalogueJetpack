6775412dbf504f206275954c268cd8c82209510d
Alvin Tandiardi <alvintandiardi@gmail.com>
2020-04-29 18:38:58 +0700

    Add Package Folder and two activity

---
+++ app/src/main/java/com/alvin/moviecataloguejetpack/ui/detail/DetailMovieActivity.kt
@@ -0,0 +1,13 @@
+package com.alvin.moviecataloguejetpack.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvin.moviecataloguejetpack.R
+
+class DetailMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}

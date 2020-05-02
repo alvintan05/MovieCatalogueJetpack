895a1259031b2793d9f02f7c551ae405c64efa82
Alvin Tandiardi <alvintandiardi@gmail.com>
2020-05-02 15:09:43 +0700

    Add Instrumental Testing

--- app/src/androidTest/java/com/alvin/moviecataloguejetpack/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/alvin/moviecataloguejetpack/ui/home/HomeActivityTest.kt
@@ -1,5 +1,92 @@
 package com.alvin.moviecataloguejetpack.ui.home
 
-import org.junit.Assert.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import com.alvin.moviecataloguejetpack.R
+import com.alvin.moviecataloguejetpack.utils.DataDummy
+import org.junit.Rule
+import org.junit.Test
 
-class HomeActivityTest
+class HomeActivityTest {
+
+    private val dummyMovies = DataDummy.generateDummyMovies()
+    private val dummyTvShows = DataDummy.generateDummyTvShows()
+
+    @get:Rule
+    var activityRule = ActivityTestRule(HomeActivity::class.java)
+
+    @Test
+    fun loadMovies() {
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movies)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyMovies.size
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailMovie() {
+        onView(withId(R.id.rv_movies)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.tv_detail_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_title)).check(matches(withText(dummyMovies[0].title)))
+        onView(withId(R.id.tv_detail_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_date)).check(matches(withText("Release time: ${dummyMovies[0].releaseDate}")))
+        onView(withId(R.id.tv_detail_length)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_length)).check(matches(withText("Length of a film: ${dummyMovies[0].runtime} min")))
+        onView(withId(R.id.tv_detail_category)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_category)).check(matches(withText(dummyMovies[0].category)))
+        onView(withId(R.id.tv_detail_rating)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_rating)).check(matches(withText(dummyMovies[0].rating.toString())))
+        onView(withId(R.id.tv_detail_synopsis)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_synopsis)).check(matches(withText(dummyMovies[0].overview)))
+        onView(withId(R.id.img_detail_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.img_detail_backdrop)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadTvShows() {
+        onView(withText("Tv Shows")).perform(click())
+        onView(withId(R.id.rv_tv_shows)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_shows)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyTvShows.size
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailTvShow() {
+        onView(withText("Tv Shows")).perform(click())
+        onView(withId(R.id.rv_tv_shows)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.tv_detail_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_title)).check(matches(withText(dummyTvShows[0].title)))
+        onView(withId(R.id.tv_detail_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_date)).check(matches(withText("Release time: ${dummyTvShows[0].releaseDate}")))
+        onView(withId(R.id.tv_detail_length)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_length)).check(matches(withText("Length of a film: ${dummyTvShows[0].runtime} min")))
+        onView(withId(R.id.tv_detail_category)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_category)).check(matches(withText(dummyTvShows[0].category)))
+        onView(withId(R.id.tv_detail_rating)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_rating)).check(matches(withText(dummyTvShows[0].rating.toString())))
+        onView(withId(R.id.tv_detail_synopsis)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_detail_synopsis)).check(matches(withText(dummyTvShows[0].overview)))
+        onView(withId(R.id.img_detail_poster)).check(matches(isDisplayed()))
+        onView(withId(R.id.img_detail_backdrop)).check(matches(isDisplayed()))
+    }
+}
